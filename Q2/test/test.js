f0041e9857f6edd704ed9f16f7bceeb4e7cf7b6e
Victor_Li <lytz@berkeley.edu>
2022-05-08 23:28:45 -0700

    submit

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
-const { groth16 } = require("snarkjs");
+const { groth16, plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -35,22 +35,22 @@ describe("HelloWorld", function () {
 
     it("Should return true for correct proof", async function () {
         //[assignment] Add comments to explain what each line is doing
-        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
+        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey"); // generates the proof and circuit output
 
-        console.log('1x2 =',publicSignals[0]);
+        console.log('1x2 =',publicSignals[0]); // print the circuit output
 
-        const editedPublicSignals = unstringifyBigInts(publicSignals);
-        const editedProof = unstringifyBigInts(proof);
-        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // unstringify the circuit output
+        const editedProof = unstringifyBigInts(proof); // unstringify the proof
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // reformat and extract the signal and inputs
     
-        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // reformat into array
     
-        const a = [argv[0], argv[1]];
-        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
-        const c = [argv[6], argv[7]];
-        const Input = argv.slice(8);
+        const a = [argv[0], argv[1]]; // extract encrypted parameters using a
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]]; // extract encrypted parameters using b
+        const c = [argv[6], argv[7]]; // extract encrypted parameters using c
+        const Input = argv.slice(8); // extract input
 
-        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true; // run verifier and assert true
     });
     it("Should return false for invalid proof", async function () {
         let a = [0, 0];
@@ -63,30 +63,73 @@ describe("HelloWorld", function () {
 
 
 describe("Multiplier3 with Groth16", function () {
+    let Verifier;
+    let verifier;
 
     beforeEach(async function () {
-        //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
+        //[assignment] Add comments to explain what each line is doing
+        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2","c":"3"}, "contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey"); // generates the proof and circuit output
+
+        console.log('1x2x3 =',publicSignals[0]); // print the circuit output
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // unstringify the circuit output
+        const editedProof = unstringifyBigInts(proof); // unstringify the proof
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // reformat and extract the signal and inputs
+    
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // reformat into array
+    
+        const a = [argv[0], argv[1]]; // extract encrypted parameters using a
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]]; // extract encrypted parameters using b
+        const c = [argv[6], argv[7]]; // extract encrypted parameters using c
+        const Input = argv.slice(8); // extract input
+
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true; // run verifier and assert true
     });
     it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
 });
 
 
 describe("Multiplier3 with PLONK", function () {
+    let Verifier;
+    let verifier;
 
     beforeEach(async function () {
-        //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("PlonkVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
+        //[assignment] Add comments to explain what each line is doing
+        const { proof, publicSignals } = await plonk.fullProve({"a":"1","b":"2","c":"3"}, "contracts/circuits/Multiplier3_plonk/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3_plonk/circuit_final.zkey"); // generates the proof and circuit output
+
+        console.log('1x2x3 =',publicSignals[0]); // print the circuit output
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // unstringify the circuit output
+        const editedProof = unstringifyBigInts(proof); // unstringify the proof
+        const calldata = await plonk.exportSolidityCallData(editedProof, editedPublicSignals); // reformat and extract the signal and inputs
+    
+        const a1 = calldata.replace(/["[\]\s]/g, "").split(',');
+
+        const a2 = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+
+        expect(await verifier.verifyProof(a1[0], [a2[1]])).to.be.true; // run verifier and assert true
     });
     it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
+        let a = [0];
+        let d = [0];
+        expect(await verifier.verifyProof(a, d)).to.be.false;
     });
 });
--- Q3/contracts/circuits/RangeProof.circom
+++ Q3/contracts/circuits/RangeProof.circom
@@ -12,4 +12,10 @@ template RangeProof(n) {
     component high = GreaterEqThan(n);
 
     // [assignment] insert your code here
+    high.in[0] <== in;
+    high.in[1] <== range[1];
+    low.in[0] <== range[0];
+    low.in[1] <== in;
+
+    out <== low.out * high.out;
 }
